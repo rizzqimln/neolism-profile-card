b9db16236a3c270bed64d7e17e103a906c47b306
rizzqimln <rizzqimaulanailhami@gmail.com>
2026-06-21 20:49:20 +0700

    feat: change interface layout, add button to change theme, add dark mode, add sun and moon icon

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function toggleDark() {
-  const isDark = document.documentElement.getAttribute("data-theme") === "dark";
-  document.documentElement.setAttribute("data-theme", isDark ? "light" : "dark");
-  localStorage.setItem("theme", isDark ? "light" : "dark"); // remembers choice
+  const isNowDark = document.documentElement.classList.toggle('dark-mode');
+  localStorage.setItem('theme', isNowDark ? 'dark' : 'light');
 }
 
-// On page load, restore saved preference
-const saved = localStorage.getItem("theme");
-if (saved) document.documentElement.setAttribute("data-theme", saved);
+const saved = localStorage.getItem('theme')
+if (saved == 'dark') document.documentElement.classList.add('dark-mode')
