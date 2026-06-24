2098173d9821787dcfcb354d3d2af59f9e989e17
rizzqimln <rizzqimaulanailhami@gmail.com>
2026-06-24 16:14:08 +0700

    feat: add parentheses to the end of code line

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 function toggleDark() {
   const isNowDark = document.documentElement.classList.toggle('dark-mode');
   localStorage.setItem('btn-theme', isNowDark ? 'dark' : 'light');
-}
 
-const saved = localStorage.getItem('btn-theme')
-if (saved == 'dark') document.documentElement.classList.add('dark-mode')
+  const saved = localStorage.getItem('btn-theme')
+  if (saved == 'dark') document.documentElement.classList.add('dark-mode');
+}
